5c9898ef8aa531852fe11587c37ea192753bc37c
luvwahraan <luvwahraan+git@pikaed.org>
2024-10-12 19:48:20 +0200

    Integrated jack player

--- JackPlayer.py
+++ JackPlayer.py
@@ -1,27 +1,91 @@
+import jack
+import queue
+import soundfile as sf
+import numpy as np
 from PyQt5.QtCore import QRunnable, QObject, QThreadPool
 from PyQt5.QtCore import pyqtSignal as Signal, pyqtSlot as Slot
-
-import os
+import threading
 
 class Signals(QObject):
     started = Signal(int)
     completed = Signal(int)
 
-"""
-Get a soundfile and plays it throut jackd
-
-TODO
-Remove implement a jack player to remove jack-play
-"""
 class JackPlayer(QRunnable):
-    def __init__(self, n, sf):
+    def __init__(self, n, sf_path, clientname='JackSoundinB', buffersize=64):
         super().__init__()
         self.n = n
-        self.soundfile = sf
+        self.soundfilename = sf_path
+        self.clientname = f"{clientname}_{self.n}"
+        self.buffersize = buffersize
         self.signals = Signals()
-        
+        self.event = threading.Event()
+
+        self.q = queue.Queue(maxsize=self.buffersize)
+
+        # jack client
+        self.client = jack.Client(self.clientname)
+        self.blocksize = self.client.blocksize
+        self.samplerate = self.client.samplerate
+        self.client.set_xrun_callback(self.xrun)
+        self.client.set_shutdown_callback(self.shutdown)
+        self.client.set_process_callback(self.process)
+
     @Slot()
     def run(self):
         self.signals.started.emit(self.n)
-        os.system(f"jack-play -un JackSoundinB_{self.n} '{self.soundfile}'")
-        self.signals.completed.emit(self.n)
+        
+        try:
+            with sf.SoundFile(self.soundfilename) as f:
+                # register ports
+                for ch in range(f.channels):
+                    self.client.outports.register(f'out_{ch + 1}')
+                
+                # fill queue for audio data
+                block_generator = f.blocks(blocksize=self.blocksize, dtype='float32', always_2d=True, fill_value=0)
+                for _, data in zip(range(self.buffersize), block_generator):
+                    self.q.put_nowait(data)
+                
+                
+                with self.client:
+                    timeout = self.blocksize * self.buffersize / self.samplerate
+                    for data in block_generator:
+                        self.q.put(data, timeout=timeout)
+                    
+                    self.q.put(None, timeout=timeout)
+                    self.event.wait()
+                
+            self.signals.completed.emit(self.n)
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def process(self, frames):
+        if frames != self.blocksize:
+            self.stop_callback('blocksize problem')
+        
+        try:
+            data = self.q.get_nowait()
+        except queue.Empty:
+            self.stop_callback('Empty buffer, increase buffersize.')
+        
+        if data is None:
+            self.stop_callback()
+ 
+        # audio data to jack ports
+        for channel, port in zip(data.T, self.client.outports):
+            port.get_array()[:] = channel
+
+    def xrun(self, delay):
+        print(f"Un xrun, delay : {delay}.")
+
+    def shutdown(self, status, reason):
+        print(f"JACK shudown, statut : {status}, raison : {reason}.")
+        self.event.set()
+
+    def stop_callback(self, msg=''):
+        if msg:
+            print(f"Error : {msg}")
+        for port in self.client.outports:
+            port.get_array().fill(0)
+        self.event.set()
+        raise jack.CallbackExit
